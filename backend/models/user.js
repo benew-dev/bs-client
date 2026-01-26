2393cbeb13ddd6a58e9b72610f62911229b4a249
Fathismael93 <fathismael@gmail.com>
2026-01-26 16:37:29 +0300

    Uninstalled bcryptjs and installed another version

--- backend/models/user.js
+++ backend/models/user.js
@@ -274,7 +274,7 @@ userSchema.pre("save", async function (next) {
     // Hasher le mot de passe avec un coût adaptatif
     // En production, on pourrait augmenter à 12 pour plus de sécurité
     const saltRounds = process.env.NODE_ENV === "production" ? 12 : 10;
-    this.password = await bcrypt.hash(this.password, { rounds: saltRounds });
+    this.password = await bcrypt.hash(this.password, saltRounds);
 
     next();
   } catch (error) {
