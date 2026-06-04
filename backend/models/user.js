dc3e8d39f187e261cb1fee700fa3d3ccdfd384a1
Fathismael93 <benew-tech@benew-dj.com>
2026-06-04 11:17:53 +0300

    Updated mongoose model user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -275,8 +275,7 @@ userSchema.pre("save", async function (next) {
     // Hasher le mot de passe avec un coût adaptatif
     // En production, on pourrait augmenter à 12 pour plus de sécurité
     const saltRounds = process.env.NODE_ENV === "production" ? 12 : 10;
-    const salt = await bcrypt.genSalt(saltRounds);
-    this.password = await bcrypt.hash(this.password, salt);
+    this.password = await bcrypt.hash(this.password, saltRounds);
 
     next();
   } catch (error) {
